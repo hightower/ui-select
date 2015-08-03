e4f64dd0ac8b00a77ccc607dab24c1178069f2b8
Nathan Herzing <nherzing@gmail.com>
2015-08-03 09:57:34 -0400

    Dropwdown should always go down, not up.

--- src/uiSelectDirective.js
+++ src/uiSelectDirective.js
@@ -21,7 +21,7 @@ uis.directive('uiSelect',
       if (angular.isDefined(tAttrs.multiple))
         tElement.append("<ui-select-multiple/>").removeAttr('multiple');
       else
-        tElement.append("<ui-select-single/>");       
+        tElement.append("<ui-select-single/>");
 
       return function(scope, element, attrs, ctrls, transcludeFn) {
 
@@ -43,7 +43,7 @@ uis.directive('uiSelect',
 
         $select.onSelectCallback = $parse(attrs.onSelect);
         $select.onRemoveCallback = $parse(attrs.onRemove);
-        
+
         //Limit the number of selections allowed
         $select.limit = (angular.isDefined(attrs.limit)) ? parseInt(attrs.limit, 10) : undefined;
 
@@ -102,7 +102,7 @@ uis.directive('uiSelect',
           }
           else
           {
-              $select.tagOnBlur = false;            
+              $select.tagOnBlur = false;
           }
         });
 
@@ -276,23 +276,23 @@ uis.directive('uiSelect',
             }
 
             // Hide the dropdown so there is no flicker until $timeout is done executing.
-            dropdown[0].style.opacity = 0;
+//            dropdown[0].style.opacity = 0;
 
             // Delay positioning the dropdown until all choices have been added so its height is correct.
-            $timeout(function(){
-              var offset = uisOffset(element);
-              var offsetDropdown = uisOffset(dropdown);
+//            $timeout(function(){
+//              var offset = uisOffset(element);
+//              var offsetDropdown = uisOffset(dropdown);
 
               // Determine if the direction of the dropdown needs to be changed.
-              if (offset.top + offset.height + offsetDropdown.height > $document[0].documentElement.scrollTop + $document[0].documentElement.clientHeight) {
-                dropdown[0].style.position = 'absolute';
-                dropdown[0].style.top = (offsetDropdown.height * -1) + 'px';
-                element.addClass(directionUpClassName);
-              }
+              // if (offset.top + offset.height + offsetDropdown.height > $document[0].documentElement.scrollTop + $document[0].documentElement.clientHeight) {
+              //   dropdown[0].style.position = 'absolute';
+              //   dropdown[0].style.top = (offsetDropdown.height * -1) + 'px';
+              //   element.addClass(directionUpClassName);
+              // }
 
               // Display the dropdown once it has been positioned.
-              dropdown[0].style.opacity = 1;
-            });
+//              dropdown[0].style.opacity = 1;
+//            });
           } else {
               if (dropdown === null) {
                 return;
